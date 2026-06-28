860857c939f92310ca9494c362a0184621bd97a8
aftab-devloper <tumhari-github-email@gmail.com>
2026-06-28 16:50:34 +0500

    fix: exclude e2e from jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from 'jest'
 import nextJest from 'next/jest.js'
 
-
 const createJestConfig = nextJest({
   dir: './',
 })
@@ -10,6 +9,7 @@ const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/e2e/'],
 }
 
 export default createJestConfig(config)
